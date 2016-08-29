f0ef2c93d1477d7285f8e7f4a5378eac757b67af
Sindy <silvasindy@gmail.com>
2016-08-29 11:58:31 -0430

    prueba commit

--- project/Gulpfile.js
+++ project/Gulpfile.js
@@ -1,37 +1,40 @@
+console.log("entrooooooooo");
 var gulp = require("gulp");
 connect = require("gulp-connect");
 historyApiFallback = require("connect-history-api-fallback");
 
-gulp.task('server', function(){
+gulp.task('server', function() {
+    console.log("server ");
   connect.server({
-    root: "/app",
-    hostname: "0.0.0.0",
-    port: "8086",
-    livereloa: true,
-    middleware: function(connect,opt){
-      return [historyApiFallback];
+    root: './app',
+    hostname: '0.0.0.0',
+    port: 8087,
+    livereload: true,
+    middleware: function(connect, opt) {
+      return [ historyApiFallback ];
     }
   });
 });
-
-var stylus = require("gulp-stylus");
-nib =require("nib");
-gulp.task('css',function(){
-  gulp.src('/app/stlyesheets/main.styl')
-  .pipe(stylus({use: nib()}))
-  .pipe(gulp.dest('./app/stlyesheets'))
-  .pipe(connect.reload());
-
+var stylus = require('gulp-stylus'),
+    nib    = require('nib');
+gulp.task('css', function() {
+  gulp.src('./app/stylesheets/main.styl')
+    .pipe(stylus({ use: nib() }))
+    .pipe(gulp.dest('./app/stylesheets'))
+    .pipe(connect.reload());
 });
 
+
 gulp.task('html', function(){
+  console.log("html ");
   gulp.src('./app/**/*.html')
   .pipe(connect.reload());
 })
 
 gulp.task('watch', function(){
-  gulp.watch(['/app/**/*.html'],['html']);
-  gulp.watch(['/apps/stylesheets/**/*.styl'],['css'])
+console.log("watch ");
+  gulp.watch(['./app/**/*.html'],['html']);
+  gulp.watch(['./app/stylesheets/**/*.styl'],['css'])
 });
 
 gulp.task('default',['server', 'watch']);
